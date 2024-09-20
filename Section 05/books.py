d198a5d77c8fa977344227409b5f24eb3c63dec6
Brian Russell <bmrussell>
2024-09-20 14:45:46 +0100

    05.81 complete

--- Section 05/books.py
+++ Section 05/books.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+# Swagger http://127.0.0.1:8000/docs
+from fastapi import Body, FastAPI
 
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
@@ -19,7 +20,6 @@ async def read_all_books():
     return BOOKS
 
 # http://127.0.0.1:8000/books/?category=maths
-
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
@@ -43,3 +43,34 @@ async def read_author_category_by_query(book_author: str, category: str):
         if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
             ret.append(book)
     return ret
+
+# Add longer endpoints after shorter ones.
+@app.get("/books/{book_title}/{book_author}/")
+async def read_authors_for_book_title(book_title: str, book_author: str):
+    ret = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('title').casefold() == book_title.casefold():
+            ret.append(book)
+    return ret
+
+
+
+@app.post("/books")
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            
+@app.delete("/books")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+            
+            
