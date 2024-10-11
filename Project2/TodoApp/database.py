e25c5813dea1e5d0066a691bf4b5b58dee102be8
Brian Russell <bmrussell>
2024-10-11 15:28:12 +0100

    12.148

--- Project2/TodoApp/database.py
+++ Project2/TodoApp/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABSE_URL = 'sqlite:///./todosapp.db'
+# SQLite
+#SQLALCHEMY_DATABSE_URL = 'sqlite:///./todosapp.db'
+#engine = create_engine(SQLALCHEMY_DATABSE_URL, connect_args={'check_same_thread': False})
 
-engine = create_engine(SQLALCHEMY_DATABSE_URL, connect_args={'check_same_thread': False})
+# Postgres
+SQLALCHEMY_DATABSE_URL = 'postgresql://postgres:adminadmin@localhost/todo-db'
+engine = create_engine(SQLALCHEMY_DATABSE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
