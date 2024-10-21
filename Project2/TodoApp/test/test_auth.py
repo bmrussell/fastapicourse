dd60e9d2ee8afee87bac74d40e9977f2a6c36b45
Brian Russell <bmrussell>
2024-10-21 16:31:52 +0100

    14.180 async testing

---
+++ Project2/TodoApp/test/test_auth.py
@@ -0,0 +1,52 @@
+import pytest
+import pytest_asyncio
+from jose import jwt
+
+from ..database import get_db
+from ..routers.auth import (
+    ALGORITHM,
+    SECRET_KEY,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+)
+from .utils import *
+
+app.dependency_overrides[get_db] = override_get_db
+
+def test_authenticate_user(test_user):
+    db = TestingSessionLocal()
+    
+    user = authenticate_user(test_user.username, 'testpassword', db)    
+    assert user is not None
+    assert user.username == test_user.username
+
+def test_authenticate_unknown_user(test_user):
+    db = TestingSessionLocal()
+    
+    user = authenticate_user("Unknown", 'testpassword', db)    
+    assert user is None
+
+def test_authenticate_wrong_password(test_user):
+    db = TestingSessionLocal()
+    
+    user = authenticate_user(test_user.username, 'wrongpassword', db)    
+    assert user is None
+
+def test_authenticate_create_token():
+    username = 'spiny'
+    id = 1
+    role = 'admin'
+    token = create_access_token(username, id, role, timedelta(minutes=20))
+    decoded = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM,options={'verify_signature': False})
+    assert decoded['sub'] == username
+    assert decoded['id'] == id
+    assert decoded['role'] == role
+
+@pytest.mark.asyncio
+async def test_get_current_user_token_valid():
+    encode = {'sub': 'spiny', 'id': 1, 'role': 'admin'}    
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    user = await get_current_user(token=token)
+    assert user == {'username': 'spiny', 'id': 1, 'user_role': 'admin'}
